cec7c95de766df2086a5d662ba715c5aeee19df4
LAffey26 <138898607+LAffey26@users.noreply.github.com>
2023-09-03 01:48:21 +0300

    fix

--- player.py
+++ player.py
@@ -2,10 +2,12 @@ import pygame
 from pygame.locals import (
     K_SPACE,
     KEYDOWN)
+
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
-        self.correct_anim = 0 
+        self.correct_anim = 0
         self.walking_on_ticks = [
             pygame.image.load("image/chicken.png"),
             pygame.image.load("image/chicken_going_1.png"),
@@ -14,21 +16,22 @@ class Player(pygame.sprite.Sprite):
         ]
         self.surf_jump = pygame.image.load("image/chicken_jump.png")
         self.surf = self.walking_on_ticks[self.correct_anim]
-        self.surf.set_colorkey((255,255,255))
+        self.surf.set_colorkey((255, 255, 255))
         self.rect = self.surf.get_rect(topleft=(300, 675))
         self.jumping = False
         self.jump_time = 0
         self.walk_animation_timer = pygame.time.get_ticks()
 
     def update(self, pressed_keys):
-        walk_animation_speed = 100        
+        walk_animation_speed = 100
 
         if pygame.time.get_ticks() - self.walk_animation_timer > walk_animation_speed:
             self.walk_animation_timer = pygame.time.get_ticks()
-            self.correct_anim = (self.correct_anim + 1) % len(self.walking_on_ticks)
+            self.correct_anim = (self.correct_anim +
+                                 1) % len(self.walking_on_ticks)
             self.surf = self.walking_on_ticks[self.correct_anim]
-            self.surf.set_colorkey((255,255,255))
-
+            self.surf.set_colorkey((255, 255, 255))
+            self
         if pressed_keys[K_SPACE] and not self.jumping:
             self.jumping = True
             self.jump_time = 60
@@ -37,7 +40,7 @@ class Player(pygame.sprite.Sprite):
             self.rect.move_ip(0, -5)
             self.jump_time -= 1
             self.surf = self.surf_jump
-            self.surf.set_colorkey((255,255,255))
+            self.surf.set_colorkey((255, 255, 255))
 
         if self.jump_time == 0:
             self.rect.move_ip(0, 5)
